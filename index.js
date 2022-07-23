8c0c3ab1deae0a7d4e6a51bbd01e0cae3cc3df68
ItzJvs <Jagtapvs9832@gmail.com>
2022-07-23 17:43:31 +0530

    done with adding cors in dev

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var express = require("express");
 var app = express();
 var router = require("./controller/routes/routes");
 var connect = require("./config/db");
+var cors = require("cors");
 require("dotenv").config();
 connect();
 let PORT = process.env.PORT || 5000;
 app.use(require("body-parser").json());
 app.use("/", router);
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`OhYeah app listening on Port : ${PORT}`);
 });
