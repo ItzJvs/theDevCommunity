facd1a7e5eed70ca562ecec56862a3286da86f2d
ItzJvs <Jagtapvs9832@gmail.com>
2022-07-24 18:17:55 +0530

    deployed the app on https://app-59a19034-e8a3-4b2c-b6a4-3f6d696e91c0.cleverapps.io/

--- view/src/App.js
+++ view/src/App.js
@@ -7,7 +7,7 @@ function App() {
   let data = {};
   useEffect(() => {
     async function getData() {
-      data = await axios.get("https://thedevcommunityclone.herokuapp.com/getAll");
+      data = await axios.get("https://app-59a19034-e8a3-4b2c-b6a4-3f6d696e91c0.cleverapps.io/getAll");
     }
     getData();
     console.log(data);
