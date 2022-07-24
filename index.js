8a7abc82e70b0d82b2524b5c9b3a34ffcfb08837
ItzJvs <Jagtapvs9832@gmail.com>
2022-07-24 21:23:58 +0530

    done with adding cors successfully ..

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ require("dotenv").config();
 connect();
 let PORT = process.env.PORT || 5000;
 app.use(require("body-parser").json());
+app.use(cors({ origin: "*", credentials: true }));
 app.use("/", router);
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 app.listen(PORT, () => {
   console.log(`OhYeah app listening on Port : ${PORT}`);
 });
--- view/src/App.js
+++ view/src/App.js
@@ -1,28 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  let data = {};
+  let [data, setData] = useState([]);
   useEffect(() => {
-    async function getData() {
-      data = await axios.get("https://app-59a19034-e8a3-4b2c-b6a4-3f6d696e91c0.cleverapps.io/getAll");
-    }
-    getData();
-    console.log(data);
+    fetch("https://app-59a19034-e8a3-4b2c-b6a4-3f6d696e91c0.cleverapps.io/getAll").then(async (res) => {
+      setData(await res.json());
+    });
   }, []);
-  console.log("data: ", data);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        {data.map((data) => {
+          return (
+            <pre>
+              Username : {data?.name} Password : {data?.password} <br />
+            </pre>
+          );
+        })}
       </header>
     </div>
   );
